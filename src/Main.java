c71b407450bf2e4dd7423906303ef4367589127e
tomato.git <https://github.com/myeongjaeking/algorithm-c->
2024-12-30 19:54:10 +0900

    [Feat] 사용자의 문자열 입력 및 구분자 기준으로 숫자 분리 기능 구현 및 분리한 각 숫자의 합 기능 구현

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        String test = "1,2,3:4";
+//        Colon colon = new Colon(test);
+//        Rest rest = new Rest(colon.getColonNotString());
+//        System.out.println("Rest Sum :"+rest.getSum());
+//        System.out.println("Colon Sum :"+colon.getSum());
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            String str = sc.next();
+            Colon colon1 = new Colon(str);
+            Rest rest1 = new Rest(colon1.getColonChangePlus());
+            SumPlus sumPlus = new SumPlus(rest1.getRestChangePlus());
+            System.out.println(sumPlus.getSum());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
         }
     }
 }
