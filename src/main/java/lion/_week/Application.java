248d9121163d63741d21385fce7a5081ef32dbaf
tomato.git <https://github.com/myeongjaeking/algorithm-c->
2024-12-30 15:59:34 +0900

    test first commit

---
+++ src/main/java/lion/_week/Application.java
@@ -0,0 +1,23 @@
+package lion._week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+
+		String str = "apple, banana, orange, grape";
+		String[] strArr1 = str.split(",");
+		String[] strArr2 = str.split(":");
+
+
+
+
+	}
+
+}
